008175a4159e9cdcfb769ef0c1cd97ae0e494316
Nikolay <meetrious@gmail.com>
2020-10-07 01:06:47 +0700

    repush, cause of some unexplained shenanigan happening

--- projects/ST/ST/ST_main.cpp
+++ projects/ST/ST/ST_main.cpp
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 #include <3rdModel_Pipe.h>
-=======
-//âûáèðàåì çàäà÷ó, êîòîðóþ áóäåì ðåøàòü
-#include <Test1D_Pipe.h>
->>>>>>> 83b0c79880d264b11dc44cc5f219e5a8f06899ba
 
 using namespace StraightTask;
 
 
 int main()
 {
-<<<<<<< HEAD
 	//calling for calculatings
 	try {ODE_solver();}
 	catch (const char* exception) {
@@ -18,12 +12,6 @@ int main()
 	} //*/
 
 	getchar();
-=======
-	//SolutionOutput();
-	
-	//calling for calculations
-	ODE_solver();
->>>>>>> 83b0c79880d264b11dc44cc5f219e5a8f06899ba
 
 	return EXIT_SUCCESS;
 }
